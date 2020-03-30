4859c7218c831c5b35a7cfab33e6ab39c3490007
root <root@dahu-7.grenoble.grid5000.fr>
2020-03-30 06:52:45 +0200

    update

---
+++ add_urls_stable.py
@@ -0,0 +1,14 @@
+import sys
+import random
+
+def main():
+     f= open("url_objects.file","w+")
+     nb = int(sys.argv[3])
+     for i in range(nb):
+         url ="http://"+ sys.argv[1] + ":31380/trip/" +  str(random.randint(1,10000)) + "/1234/4567/65431"
+         f.write(url + " " + sys.argv[2] +"\n")
+     f.close()
+
+
+if __name__== "__main__":
+  main()
