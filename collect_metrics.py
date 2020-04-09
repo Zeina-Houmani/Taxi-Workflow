4242dc4b81580cced8dc65993a178ccd4805798b
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-09 02:37:38 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -172,6 +172,7 @@ def get_CPU_usage(POD_NAME, NAMESPACE):
   TIME = datetime.now().isoformat()
   #TIME= dt.strftime("%Y-%m-%dT%H:%M:%S")
   TIME = TIME + "Z"
+  print TIME
   #TIME = "2020-04-09T01:44:50Z"
   QUERY =  'sum(rate(container_cpu_usage_seconds_total{pod_name!="", image!="", pod_name=~"' + POD_NAME + '.*", namespace=~"' + NAMESPACE + '"}[5m])) by (pod_name)'
   response = requests.get(PROMETHEUS_URL + QUERY_API, params={'query': QUERY, 'time': TIME})
