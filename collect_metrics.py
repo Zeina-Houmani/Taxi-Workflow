fdb751b3e5f27ff50070ed89430be44c9f014d8b
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-09 17:45:46 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -137,7 +137,7 @@ def get_static_metrics():
 			for pod in pod_list.items:
 				dynamic =  OrderedDict()
 				pod_name = pod.metadata.name
-				dynamic['instance'] = counter + "/" + replicas_count
+				dynamic['instance'] = str(counter) + "/" + str(replicas_count)
 				counter = counter + 1
 				dynamic['CPU usage'] = get_CPU_usage(pod_name,namespace_name)
 				dynamic['RAM usage'] = get_RAM_usage(pod_name,namespace_name)
