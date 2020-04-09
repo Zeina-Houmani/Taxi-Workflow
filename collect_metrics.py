7e74827eeb9f18a24be4d1750df858d4de9981ca
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-09 14:52:39 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -196,7 +196,7 @@ def get_CPU_usage(POD_NAME, NAMESPACE):
   results = response.json()['data']['result']	
   print results
   print "***********"
-  print results[0]
+  print results[0].get('value')
   #return "%.2f" % float(results['value'])
 			
 	
