c6fbb7fff4cff53fbb48f75ea2e8da65eef32b8e
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-12 15:33:54 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -73,15 +73,15 @@ def get_server_metrics():
 	
 	#memory usage 
 	QUERY_USAGE_memory = 'sum(container_memory_working_set_bytes{id="/",kubernetes_io_hostname=~"' +NODE_NAME + '"})'
-	usage_metrics['Memory used'] = get_memory_usage(QUERY_USAGE_memory, MEMORY_CAPACITY)
+	usage_metrics['RAM usage'] = get_memory_usage(QUERY_USAGE_memory, MEMORY_CAPACITY)
 	
 	#cpu usage 
 	QUERY_USAGE_cpu = 'sum(rate(container_cpu_usage_seconds_total{id="/",kubernetes_io_hostname=~"' + NODE_NAME + '"}[5m]))'			
-	usage_metrics['cpu used'] =get_cpu_usage(QUERY_USAGE_cpu, CPU_CAPACITY)
+	usage_metrics['CPU usage'] =get_cpu_usage(QUERY_USAGE_cpu, CPU_CAPACITY)
 	
 	#disk usage 
 	QUERY_USAGE_disk =  'sum(container_fs_usage_bytes{device=~"^/dev/sda.$",id="/",kubernetes_io_hostname=~"' + NODE_NAME + '"})'
-	usage_metrics['disk used'] = get_disk_usage(QUERY_USAGE_disk, DISK_CAPACITY)
+	usage_metrics['disk usage'] = get_disk_usage(QUERY_USAGE_disk, DISK_CAPACITY)
 	
 	#Network usage
 	network =  OrderedDict()
@@ -150,7 +150,6 @@ def get_service_metrics():
 				counter = counter + 1
 				dynamic['server'] = pod.spec.node_name
 			
-	
 				QUERY_USAGE_cpu =  'sum(rate(container_cpu_usage_seconds_total{pod_name!="", image!="", pod_name=~"' + pod_name + '"}[5m]))'
 				dynamic['CPU usage'] = get_cpu_usage(QUERY_USAGE_cpu, float (total_limit_cpu)/ 1000)
 				
@@ -159,9 +158,6 @@ def get_service_metrics():
 					
 				QUERY_USAGE_disk ='sum(container_fs_usage_bytes{pod_name!="", image!="", pod_name=~"' + pod_name + '"})'
 				dynamic['disk usage'] =  get_disk_usage(QUERY_USAGE_disk, total_limit_disk * 1024 * 1024 * 1024)
-				#DISK_USAGE = get_query_result(QUERY_USAGE_disk)[0].get('value')[1]
-				#QUERY_USAGE_disk_percentage = "%.2f" % float(( float(DISK_USAGE) / float(total_limit_disk * 1024 * 1024 * 1024)) * 100) 
-				#dynamic['disk usage'] = str(humanbytes(DISK_USAGE))  + " (" + str(QUERY_USAGE_disk_percentage) + "%)"
 				
 				#Network usage
 				network =  OrderedDict()
