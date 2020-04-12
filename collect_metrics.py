cddce1bceeb03aeb9e04e3e1778407abfd10f039
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-11 22:46:09 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -149,6 +149,7 @@ def get_service_metrics():
 				counter = counter + 1
 				dynamic['server'] = pod.spec.node_name
 			
+				print total_limit_disk
 	
 				QUERY_USAGE_cpu =  'sum(rate(container_cpu_usage_seconds_total{pod_name!="", image!="", pod_name=~"' + pod_name + '.*", namespace=~"' + namespace_name + '"}[5m])) by (pod_name)'
 				CPU_USAGE = get_query_result(QUERY_USAGE_cpu)[0].get('value')[1]
