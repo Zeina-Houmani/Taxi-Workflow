8974fe469337864e183f3d2f1b670a292c752983
Zeina-Houmani <46556854+Zeina-Houmani@users.noreply.github.com>
2020-04-12 02:20:26 -0400

    Update collect_metrics.py

--- collect_metrics.py
+++ collect_metrics.py
@@ -188,7 +188,9 @@ def get_cpu_usage(QUERY_USAGE_cpu, CPU_CAPACITY):
 	#usage_metrics['cpu used'] = str ("%.2f" % float(CPU_USAGE)) + " (" + str(QUERY_USAGE_cpu_percentage) + ")"
 	CPU_USAGE = get_query_result(QUERY_USAGE_cpu)[0].get('value')[1]
 	QUERY_USAGE_cpu_percentage = (float(CPU_USAGE) / (float (CPU_CAPACITY))*100
-	PERCENTAGE = str("%.2f" % float(CPU_USAGE)) + " (" + str("%.2f" % QUERY_USAGE_cpu_percentage) + "%)"	
+	#PERCENTAGE = str("%.2f" % float(CPU_USAGE)) + " (" + str("%.2f" % QUERY_USAGE_cpu_percentage) + "%)"	
+	PERCENTAGE = ""
+	PERCENTAGE =  str ("%.2f" % float(CPU_USAGE)) + " (" + str(  "%.2f" % QUERY_USAGE_cpu_percentage) + "%)"
 	return PERCENTAGE
 	
 	
