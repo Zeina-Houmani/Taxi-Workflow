ba802a822bfa0b03180736d5add85e5ae6ad042b
HOUMANI Zeina <zeina.houmani@inria.fr>
2020-04-13 03:25:00 -0400

    update

--- load.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://mirror.filearena.net/pub/speed/SpeedTest_2048MB.dat?_ga=2.214932319.973892181.1585884968-607421180.1585884968'
-
-for t in  range(50):
-      print('Download a file ... ')
-      r = requests.get(url)
